110d7132c87710fba0da85c9576b929bd57d69ab
Rony <rosaroli558@gmail.com>
2026-03-13 00:27:01 +0800

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,1 +1,12 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyBQI0NKBfJ2U1Kmu1FVEE-o-IkkjCPOd30",
+  authDomain: "scoutfind-72168.firebaseapp.com",
+  projectId: "scoutfind-72168",
+  storageBucket: "scoutfind-72168.firebasestorage.app",
+  messagingSenderId: "1046529133271",
+  appId: "1:1046529133271:web:3a368ed5830c7816267e92",
+  measurementId: "G-1112ZHQFEV"
+};
+export const app = initializeApp(firebaseConfig);
